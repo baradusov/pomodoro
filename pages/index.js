98398d11a760635d054f2bb4c0d4e1d33a57c60f
Nuril Baradusov <baradusovnh@gmail.com>
2022-04-09 16:25:05 +0300

    Added todo list

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import Head from 'next/head';
 import { observer } from 'mobx-react-lite';
+import cn from 'clsx';
 
 import { useStore } from 'lib/hooks/useStore';
 
 import styles from 'styles/Home.module.css';
-import { settings } from 'lib/store/tomato';
 
 const Home = () => {
   const {
@@ -18,9 +18,13 @@ const Home = () => {
       isLongBreak,
       isPaused,
       remained,
+      sessions,
     },
+    tasks: { tasks, addNewTask },
     audio,
   } = useStore();
+
+  const inputRef = useRef(null);
 
   const seconds = time / 1000;
 
@@ -59,9 +63,21 @@ const Home = () => {
     return 'working';
   };
 
+  const handleAddNewTask = (event) => {
+    if (event.key === 'Enter' && event.target.value.trim() !== '') {
+      addNewTask(event.target.value);
+
+      inputRef.current.value = '';
+    }
+  };
+
+  const handleCompleteTask = (task) => {
+    task.toggleCompleteTask();
+  };
+
   useEffect(() => {
     audio.setAudio(new Audio('/ding.wav'));
-  }, [audio.setAudio]);
+  }, [audio]);
 
   return (
     <>
@@ -69,7 +85,41 @@ const Home = () => {
         <title>{timer}</title>
       </Head>
       <main className={styles.main}>
-        <p className={styles.timer}>{timer}</p>
+        <div className={styles.timer}>
+          <p>{timer}</p>
+        </div>
+
+        <div className={styles.tasks}>
+          <p className={styles.taskTitle}>Plan</p>
+
+          <ul className={styles.taskList}>
+            {Array.from(tasks).map(([id, task]) => {
+              const classes = cn(
+                styles.task,
+                task.completed && styles.completed
+              );
+
+              return (
+                <li
+                  className={classes}
+                  key={id}
+                  onClick={() => handleCompleteTask(task)}
+                >
+                  <p>{task.text}</p>
+                  <p>{task.tomatos}</p>
+                </li>
+              );
+            })}
+          </ul>
+
+          <input
+            className={styles.input}
+            ref={inputRef}
+            type="text"
+            placeholder="Enter new task"
+            onKeyDown={handleAddNewTask}
+          />
+        </div>
 
         <div className={styles.buttons}>
           <button className={styles.button} onClick={toggleState}>
@@ -79,7 +129,7 @@ const Home = () => {
 
         <div className={styles.info}>
           <p>
-            passed: {current} of {settings.sessions}
+            completed {current} of {sessions} tomatos
           </p>
           <p>{renderBreakInfo()}</p>
         </div>
